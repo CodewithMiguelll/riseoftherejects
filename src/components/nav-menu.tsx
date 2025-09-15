32d8ea9dccb3475aa16d94b103a5f88b83db2423
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-09-15 18:50:02 +0100

    feat(navbar): setup navbar for the site.

---
+++ src/components/nav-menu.tsx
@@ -0,0 +1,61 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+const Navigation = () => {
+  return (
+    <div className="flex justify-center my-6 text-[#121212] font-medium gap-8">
+      <NavigationMenu className="mx-4 flex">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/"
+            >
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/characters"
+            >
+              Characters
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/factions"
+            >
+              Factions
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/about"
+            >
+              About
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/timeline"
+            >
+              Timeline
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+
+export default Navigation;
