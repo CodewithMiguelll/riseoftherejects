b29f9b622d4dceaa23aa63b684bb1c2a7380b9ee
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-09-25 20:11:16 +0100

    feat(metadata): set up layout file for seo and metadata.

---
+++ src/app/timeline/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Timeline | Rise of the Rejects",
+  description:
+    "Explore the timeline of events that shaped the world of ROTR.",
+};
+
+export default function TimelineLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
