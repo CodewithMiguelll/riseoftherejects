271bed0d677f789b0da5bf9a036a9e928ab691a7
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-09-25 20:10:12 +0100

    feat(metadata): set up layout file for seo and metadata.

---
+++ src/app/characters/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Characters | Rise of the Rejects",
+  description:
+    "Explore the diverse characters that inhabit the world of ROTR.",
+};
+
+export default function CharactersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
