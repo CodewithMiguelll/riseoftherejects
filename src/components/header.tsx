eb130fb4eb84ae2ac07e8d9f4641eb45446c4eff
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-09-25 20:13:42 +0100

    feat(header): added sticky functionality to the header for easier navigation.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Pacifico, Rubik_Glitch } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { Menu, X } from "lucide-react"; // hamburger & close icons
+import { Menu, X } from "lucide-react";
 import Navigation from "@/components/nav-menu";
 
 const pacifico = Pacifico({
@@ -23,12 +22,12 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="w-full shadow-md p-2.5">
+    <header className="w-full sticky top-0 z-50 bg-neutral-950/90 backdrop-blur-md shadow-md p-2.5">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         {/* Logo */}
         <Link
           href="/"
-          className={`${rubik_glitch.className} text-xl font-bold text-black`}
+          className={`${rubik_glitch.className} text-xl font-bold text-white`}
         >
           RISE OF THE REJECTS
         </Link>
@@ -42,9 +41,9 @@ export default function Header() {
         <div className="md:hidden">
           <button onClick={() => setIsOpen(!isOpen)} aria-label="Toggle menu">
             {isOpen ? (
-              <X className="text-[#121212] w-7 h-7 cursor-pointer" />
+              <X className="text-white w-7 h-7 cursor-pointer" />
             ) : (
-              <Menu className="text-[#121212] w-7 h-7 hover:cursor-pointer" />
+              <Menu className="text-white w-7 h-7 hover:cursor-pointer" />
             )}
           </button>
         </div>
@@ -52,9 +51,8 @@ export default function Header() {
 
       {/* Mobile Dropdown */}
       {isOpen && (
-        <div className="md:hidden flex flex-col items-center gap-4 pb-4 px-1.5">
+        <div className="md:hidden flex flex-col items-center gap-4 pb-4 px-1.5 text-white">
           <Navigation />
-
         </div>
       )}
     </header>
