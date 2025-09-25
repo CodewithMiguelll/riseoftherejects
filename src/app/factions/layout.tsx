f0e293ab1c0124b9b675442d5f3c14ea65b7c52b
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-09-25 20:10:40 +0100

    feat(metadata): set up layout file for seo and metadata.

---
+++ src/app/factions/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Factions | Rise of the Rejects",
+  description:
+    "Discover the factions vying for power in the world of ROTR.",
+};
+
+export default function FactionsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
