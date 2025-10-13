e2c46763281c197de6caedbf919966428f947e9d
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-10-13 21:07:22 +0100

    feat(characters): set up fonts in the characters page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,9 +1,38 @@
+import { Orbitron, Barlow_Condensed, Source_Sans_3 } from "next/font/google";
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
+
+// Subheading Font
+const barlow = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+// Body Font
+const sourceSans = Source_Sans_3({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 const CharactersPage = () => {
-    return ( 
-        <>
-        <h1>Characters Page</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <h1
+        className={`${orbitron.className} text-2xl md:text-6xl font-bold`}
+      >
+        Mavericks Of The State
+      </h1>
+      <p
+        className={`${sourceSans.className} text-lg md:text-xl mt-2.5 font-medium text-pretty`}
+      >
+        Meet the defiant Rejects, the resilient Lowers, and the authoritative
+        Uppers in a world divided by power and rebellion.
+      </p>
+    </>
+  );
+};
+
 export default CharactersPage;
