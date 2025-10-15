e6b0bb90b7531eeeaf6995a7799925b2ae3aa6dc
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-10-15 18:40:11 +0100

    feat(footer): implement hover states for website footer Updated background color and padding for better alignment and visual consistency.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -32,25 +32,25 @@ export default function Footer() {
         <div className="flex flex-wrap justify-center md:justify-end gap-6 text-sm font-medium text-neutral-200">
           <Link
             href="/about"
-            className="hover:text-white transition cursor-none"
+            className="hover:bg-[#c4b6c1] text-[#efebee] hover:text-[#060506] transition cursor-none p-3 rounded-md"
           >
             About
           </Link>
           <Link
             href="/characters"
-            className="hover:text-white transition cursor-none"
+            className="hover:bg-[#c4b6c1] text-[#efebee] hover:text-[#060506] transition cursor-none p-3 rounded-md"
           >
             Characters
           </Link>
           <Link
             href="/factions"
-            className="hover:text-white transition cursor-none"
+            className="hover:bg-[#c4b6c1] text-[#efebee] hover:text-[#060506] transition cursor-none p-3 rounded-md"
           >
             Factions
           </Link>
           <Link
             href="/timeline"
-            className="hover:text-white transition cursor-none"
+            className="hover:bg-[#c4b6c1] text-[#efebee] hover:text-[#060506] transition cursor-none p-3 rounded-md"
           >
             Timeline
           </Link>
@@ -59,7 +59,7 @@ export default function Footer() {
         {/* Right: CTA */}
         <div className="flex justify-center md:justify-end">
           <Link href="/factions">
-            <button className="px-4 py-2 bg-[#9333ea] text-[#e5e7e8] rounded-lg font-semibold shadow hover:bg-[#9233eac9] transition cursor-none">
+            <button className="px-4 py-2 bg-[#c4b6c1] text-[#060506] hover:bg-[#c4b6c1cc] rounded-lg font-semibold shadow transition cursor-none">
               Join the Cause
             </button>
           </Link>
